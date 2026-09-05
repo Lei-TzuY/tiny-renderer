573c3e91bf32be5dd20a7f50114fea7d7d61ed91
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-06 01:40:20 +0800

    feat: define owned material texture batches

--- include/tiny_renderer/obj_loader.hpp
+++ include/tiny_renderer/obj_loader.hpp
@@ -3,12 +3,14 @@
 #include <cstddef>
 #include <filesystem>
 #include <istream>
+#include <memory>
 #include <stdexcept>
 #include <string>
 #include <vector>
 
 #include "tiny_renderer/material.hpp"
 #include "tiny_renderer/mesh.hpp"
+#include "tiny_renderer/texture.hpp"
 
 namespace tiny_renderer {
 
@@ -28,8 +30,16 @@ struct MaterialBatch {
     MaterialState material{};
 };
 
+struct MaterialAssetBatch {
+    Mesh mesh;
+    std::string material_name;
+    MaterialState material{};
+    std::shared_ptr<const Texture2D> diffuse_texture;
+};
+
 [[nodiscard]] Mesh load_obj(std::istream& input);
 [[nodiscard]] Mesh load_obj_file(const std::filesystem::path& path);
 [[nodiscard]] std::vector<MaterialBatch> load_obj_material_batches_file(const std::filesystem::path& path);
+[[nodiscard]] std::vector<MaterialAssetBatch> load_obj_material_asset_batches_file(const std::filesystem::path& path);
 
 }  // namespace tiny_renderer
