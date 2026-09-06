5fbcfd94912c574df6b18c97eed3bdd78d8707be
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-07 03:57:47 +0800

    Fix shadow renderer header ownership

--- include/tiny_renderer/shadow_renderer.hpp
+++ include/tiny_renderer/shadow_renderer.hpp
@@ -5,9 +5,7 @@
 #include <span>
 
 #include "tiny_renderer/model_renderer.hpp"
-#include "tiny_renderer/point_shadow.hpp"
 #include "tiny_renderer/shadow.hpp"
-#include "tiny_renderer/spot_shadow.hpp"
 
 namespace tiny_renderer {
 
@@ -36,31 +34,5 @@ render_directional_shadow_cascades(
     const Mat4& camera_view,
     std::span<const DirectionalShadowCascadeCapture> cascades,
     DirectionalShadowMapOptions options);
-
-struct PointShadowCubemapOptions {
-    std::size_t size{0U};
-    float near_plane{0.1F};
-    float far_plane{100.0F};
-    CullMode cull_mode{CullMode::None};
-    FrontFace front_face{FrontFace::CounterClockwise};
-};
-
-[[nodiscard]] std::shared_ptr<const DepthCubemap> render_point_shadow_cubemap(
-    std::span<const PreparedModelListEntry> entries,
-    const Vec3& light_position,
-    PointShadowCubemapOptions options);
-
-struct SpotShadowMapOptions {
-    std::size_t size{0U};
-    float near_plane{0.1F};
-    float far_plane{100.0F};
-    CullMode cull_mode{CullMode::None};
-    FrontFace front_face{FrontFace::CounterClockwise};
-};
-
-[[nodiscard]] std::shared_ptr<const SpotShadowMap> render_spot_shadow_map(
-    std::span<const PreparedModelListEntry> entries,
-    const SpotLight& light,
-    SpotShadowMapOptions options);
 
 }  // namespace tiny_renderer
